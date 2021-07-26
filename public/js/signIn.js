951411c49b8207b905f7ed58605b3cd22c725974
Krish <krpxbox@gmail.com>
2021-07-26 17:43:08 +0000

    redirect

--- public/js/signIn.js
+++ public/js/signIn.js
@@ -2,5 +2,15 @@ const signInButton = document.querySelector(".button");
 console.log(signInButton);
 
 signInButton.addEventListener("click", ()=> {
-    console.log("clicked!")
+    console.log("clicked!");
+    const provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider)
+    .then(result => {
+        const user = result.user;
+        console.log("login success", user);
+        window.location = "writeNote.html"
+    })
+    .catch(error => {
+        console.log("failed login");
+    });
 })
